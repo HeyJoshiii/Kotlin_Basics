d95b3c9d6d5a5b556728e2c48e14ce66d2b63535
Joshua <Joshua.Gerhard@Freenet.de>
2023-02-03 17:53:20 +0200

    init: Initial Commit

---
+++ app/src/main/java/com/example/kotlinbasics/ui/theme/HomeScreen.kt
@@ -0,0 +1,26 @@
+package com.example.kotlinbasics.ui.theme
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            HomeAppBar()
+        },
+        content = {
+
+        }
+    )
+}
+
+@Composable
+fun HomeAppBar() {
+    Row {
+        Text(text = "Home")
+    }
+}
---
+++ app/src/main/java/com/example/kotlinbasics/ui/theme/LoginScreen.kt
@@ -0,0 +1,96 @@
+package com.example.kotlinbasics.ui.theme
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+/**
+ * Enum Class that contains routes to navigate
+ */
+enum class LoginScreen() {
+    Login,
+    Home
+}
+
+@Composable
+fun LoginScreen(
+    modifier: androidx.compose.ui.Modifier
+){
+    val username = remember { mutableStateOf("Joshiii")}
+    val password = remember { mutableStateOf("Joshiii")}
+    val navController = rememberNavController()
+
+    Column(
+        modifier = modifier.padding(28.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+
+    ){
+        Icon(
+            painter = rememberVectorPainter(Icons.Filled.Person),
+            contentDescription = "login_image",
+            modifier = Modifier.fillMaxWidth().size(150.dp)
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = username.value,
+            onValueChange = {text -> username.value = text},
+            label = { Text(text = "Username")},
+            shape = RoundedCornerShape(corner = CornerSize(20.dp))
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = "",
+            onValueChange = {passwordString -> password.value = passwordString},
+            label = { Text(text = "Password")},
+            visualTransformation = PasswordVisualTransformation(),
+            shape = RoundedCornerShape(corner = CornerSize(20.dp))
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+        
+        Button(
+            modifier = Modifier.fillMaxWidth().height(60.dp),
+            onClick = {},
+            shape = RoundedCornerShape(corner = CornerSize(20.dp))
+        ) {
+            
+        }
+    }
+
+    NavHost(
+        navController = navController,
+        startDestination = LoginScreen.Login.name,
+        modifier = modifier.padding(innerPadding)
+    ) {
+        composable(route = LoginScreen.Login.name) {
+            HomeScreen(
+                    navController.navigate(LoginScreen.Home.name)
+            )
+        }
+    }
+}
